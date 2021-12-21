b78b9e3ae9075508b0cac3641cf40298d1e88035
Jhelison <jhelison@weef.com>
2021-12-21 17:24:41 -0300

    feat(repositories): Add the repository interface

---
+++ src/Repositories/Core/IRepository.fs
@@ -0,0 +1,10 @@
+namespace Core
+
+open System.Collections.Generic
+
+type IRepository<'T> =
+    abstract member All: IEnumerable<'T>
+    abstract member Get: int -> 'T
+    abstract member Add: 'T -> 'T
+    abstract member Delete: int -> unit
+    abstract member Save: unit
