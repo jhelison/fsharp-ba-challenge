3c39144c4dcb1c6e8bbe5583b4d74c55a3222967
Jhelison <jhelison@weef.com>
2021-12-21 17:11:58 -0300

    feat(repositories): Add the DataContext class

---
+++ src/Repositories/Persistence/DataContext.fs
@@ -0,0 +1,20 @@
+namespace Persistence
+
+open Microsoft.EntityFrameworkCore
+open Npgsql
+
+open Shared
+
+type DataContext() =
+    inherit DbContext()
+
+    [<DefaultValue>]
+    val mutable MarketData: DbSet<MarketData>
+
+    member this.marketData
+        with public get () = this.MarketData
+        and public set m = this.MarketData <- m
+
+    override __.OnConfiguring(optionBuilder: DbContextOptionsBuilder) =
+        optionBuilder.UseNpgsql(@"Host=localhost:5432;Username=postgres;Password=postgres;Database=postgres")
+        |> ignore
